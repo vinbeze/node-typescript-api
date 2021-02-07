562736625a98b94aa7849f3c9389d45da4defb78
Vinicius B <vinicius_bezerra@hotmail.com>
2021-02-07 19:03:11 -0300

    setup Server e adicionado primeira rota

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,10 +1,12 @@
-import supertest from 'supertest';
+import { Controller, Get} from "@overnightjs/core";
+import {Request, Response} from "express";
 
-describe('beach forecast functional tests', () => {
-    it("should return a forecast with just a few times", async() => {
-        const {body,status} = await global.testRequest.get('/forecast');
-        expect(status).toBe(200);
-        expect(body).toEqual([{
+@Controller('forecast')
+export class ForecastController{
+
+    @Get('')
+    public getForecastForLoggedUser(_:Request, res: Response): void {
+        res.send([{
             "time": "2020-04-26T00:00:00+00:00",
             "forecast": [{
               "lat": -33.792726,
@@ -37,5 +39,6 @@ describe('beach forecast functional tests', () => {
               "windDirection": 310.48
             }] }
           ]);
-    });
-});
+    }
+
+}
