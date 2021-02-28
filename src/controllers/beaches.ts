501e83973c55a4e381d9f9fe6eeebbe75314aff7
Vinicius B <vinicius_bezerra@hotmail.com>
2021-02-28 10:59:04 -0300

    criando model de beach

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,23 @@
 import { Controller, Get, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
+import moongose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController{
     @Post('')
     public async create(req: Request,res: Response): Promise<void>{
-        res.status(201).send( { ... req.body, id: 'fake-id'}  );
+        try{
+        const beach = new Beach(req.body);
+        const result = await beach.save();
+        res.status(201).send(result);
+        }catch(error){
+            if(error instanceof moongose.Error.ValidationError) { //checando se o erro é do moongoso
+                res.status(422).send ({ error: error.message});
+            } else {
+                res.status(500).send ({ error: 'Internal Server Error'});
+            }
+
+        }
     }
 }
