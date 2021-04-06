41349d0f37417438c4ee5100deaf71026343dd9e
Vinicius B <vinicius_bezerra@hotmail.com>
2021-04-06 17:05:41 -0300

    Adicionado encrypt na senha do usuario

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,4 +1,5 @@
 import { User } from "@src/models/user";
+import AuthService from "@src/services/auth";
 
 describe('Users functional tests',()=> {
 
@@ -7,7 +8,7 @@ describe('Users functional tests',()=> {
     });
 
     describe('when creating a new user',()=>{
-        it('should successfully create a new user', async()=>{
+        it('should successfully create a new user with encrypted password', async()=>{
             const newUser = {
                 name: 'John Doe',
                 email: 'John@mail.com',
@@ -16,7 +17,9 @@ describe('Users functional tests',()=> {
 
             const response = await global.testRequest.post('/users').send(newUser);
             expect(response.status).toBe(201);
-            expect(response.body).toEqual(expect.objectContaining(newUser));
+            await expect(AuthService.comparePassword(newUser.password,response.body.password)).resolves.toBeTruthy();
+            expect(response.body).toEqual(expect.objectContaining( { ... newUser, ... {password : expect.any(String)} } ) );
+
 
         });
         it('should return 422 when there is a validantion error', async()=>{
