e3772c3b5c32bd1cc3657de180d3e32d9a7fc460
Vinicius B <vinicius_bezerra@hotmail.com>
2021-04-06 16:28:59 -0300

    Módulo de Users - Mongoose hooks, validation e Type narrowing

---
+++ src/controllers/index.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_VALIDATION } from "@src/models/user";
+import { Response } from "express";
+import mongoose  from "mongoose";
+
+
+export abstract class BaseController{
+    protected sendCreateUpdateErrorResponse(res: Response, error: mongoose.Error.ValidationError| Error): void {
+    
+        if (error instanceof mongoose.Error.ValidationError) {
+            const clientErrors = this.handleClientErrors(error);
+            res.status(clientErrors.code).send({code: clientErrors.code, error: clientErrors.error});
+          } else {
+            res.status(500).send({ code: 500, error: 'Internal Server Error' });
+          }
+    };
+
+    private handleClientErrors(error: mongoose.Error.ValidationError): {code: number; error: string} {
+        const duplicatedErrors = Object.values(error.errors).filter((err) => err.kind === CUSTOM_VALIDATION.DUPLICATED);
+        if(duplicatedErrors.length) { 
+            return {code: 409, error: error.message} ;
+        }else {
+            return { code: 422 ,error: error.message } ; 
+        }
+    }
+
+
+}
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -19,5 +19,37 @@ describe('Users functional tests',()=> {
             expect(response.body).toEqual(expect.objectContaining(newUser));
 
         });
+        it('should return 422 when there is a validantion error', async()=>{
+            const newUser = {
+                email: 'John@mail.com',
+                password: '1234'
+            };
+
+            const response = await global.testRequest.post('/users').send(newUser);
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+                code: 422,
+                error: 'User validation failed: name: Path `name` is required.',
+            });
+
+        });   
+        
+        it('should return 409 when the email already exists', async()=>{
+            const newUser = {
+                name: 'John Doe',
+                email: 'John@mail.com',
+                password: '1234'
+            };
+
+            await global.testRequest.post('/users').send(newUser);
+            const response = await global.testRequest.post('/users').send(newUser);
+            expect(response.status).toBe(409);
+            expect(response.body).toEqual({
+                code: 409,
+                error: 'User validation failed: email: already exists in the database.',
+            });
+
+        });
+
     });
 });
