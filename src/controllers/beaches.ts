0a0e33d8d2875c20d2bc65854169f3b56e5ad377
Vinicius <vinicius_bezerra@hotmail.com>
2021-04-17 07:21:12 -0300

    adicionado logger ao projeto

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,4 +1,5 @@
 import { ClassMiddleware, Controller, Get, Post } from '@overnightjs/core';
+import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
@@ -18,6 +19,7 @@ export class BeachesController {
         //checando se o erro é do moongoso
         res.status(422).send({ error: error.message });
       } else {
+        logger.error(error)
         res.status(500).send({ error: 'Internal Server Error' });
       }
     }
